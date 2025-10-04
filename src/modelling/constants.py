84fcc8c6ab99f4396aaade35e3528e8a941e84b8
Leo Burgund <leo.burgund@gmail.com>
2025-10-04 16:01:12 +0200

    refactor: split train.py into 3 files

---
+++ src/modelling/constants.py
@@ -0,0 +1,7 @@
+"""Shared constants for the modelling package."""
+
+TARGET = "popularity"
+GENRE = "track_genre"
+DATA_DIR = "./src/data"
+BAGGING_MODELS = {"rf", "et"}
+BOOSTING_MODELS = {"xgb", "cat"}
